8e34ace941f2cfe87b3cc471adc3a60d3f749cd4
AK <arwin@kasnady.com>
2022-02-09 16:57:56 +0700

    Initial Spring Starter Project setup

---
+++ src/main/java/com/kasnady/returnapi/testreturnapi/TestReturnapiApplication.java
@@ -0,0 +1,13 @@
+package com.kasnady.returnapi.testreturnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReturnapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReturnapiApplication.class, args);
+	}
+
+}
